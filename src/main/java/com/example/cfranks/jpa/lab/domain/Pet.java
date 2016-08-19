91c267633160fb1117b9790a1d84dda6098950b5
Chase Franks <clf112358@gmail.com>
2016-08-19 18:03:41 -0500

    add JPA @Entity annotations to turn domain objects into entities

--- src/main/java/com/example/cfranks/jpa/lab/domain/Pet.java
+++ src/main/java/com/example/cfranks/jpa/lab/domain/Pet.java
@@ -1,7 +1,14 @@
 package com.example.cfranks.jpa.lab.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Pet {
 
+	@Id
+	private int id;
+	
 	private String name;
 	private PetOwner owner;
 
--- src/main/java/com/example/cfranks/jpa/lab/domain/PetOwner.java
+++ src/main/java/com/example/cfranks/jpa/lab/domain/PetOwner.java
@@ -2,7 +2,14 @@ package com.example.cfranks.jpa.lab.domain;
 
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class PetOwner {
+	
+	@Id
+	private int id;
 	
 	private String firstName;
 	private String lastName;
